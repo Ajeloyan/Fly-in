6d3939fb65d89ccd7cc4ab1f3e64f9063c790ea5
Armenag Jeloyan <a.jeloyan1@icloud.com>
2026-09-17 17:52:18 +0200

    simu ok, generate drones and turns, no visu

--- flyin/simulation.py
+++ flyin/simulation.py
@@ -7,13 +7,19 @@ class Simulation:
         self.drones = drones
         self.path = path
         self.all_finished = False
+        self.progress = {drone: 1 for drone in self.drones}
 
     def launch(self):
-        for i in range(1, len(self.path)):
+        while not all(self.progress[drone] >= len(self.path) for drone in self.drones):
+            turn = []
             for drone in self.drones:
-                zone = self.path[i]
+                if self.progress[drone] >= len(self.path):
+                    continue
+                idx = self.progress[drone]
+                zone = self.path[idx]
                 if zone.has_capacity():
-                    print(f"D{drone.drone_id}-{zone.name}")
+                    turn.append(f"D{drone.drone_id}-{zone.name}")
                     drone.current_zone.remove_drone(drone)
                     zone.add_drone(drone)
-
+                    self.progress[drone] += 1
+            print(" ".join(turn))
